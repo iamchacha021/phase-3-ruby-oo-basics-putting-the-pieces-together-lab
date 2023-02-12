e007fcc59e1bb2f4a03e6157462c6dfba9a05a2c
iamchacha021 <iamchacha021@gmail.com>
2023-02-12 17:33:12 +0300

    done

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,19 @@
+require 'pry'
+class Book
+    attr_accessor :title, :author, :page_count, :genre
+    def initialize(title, author = "Agatha Christie", page_count = 272, genre = "Mystery")
+        @title = title
+        @author = author
+        @page_count = page_count 
+        @genre = genre
+    end
 
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
 
+book = Book.new("And Then There Were None", "Agatha Christie", 272, "Mystery")
+book.title
+
+# binding.pry
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,21 @@
 # Make your shoe class here!
+require 'pry'
+class Shoe
+    attr_accessor :brand, :color, :size, :material, :condition
+
+    def initialize(brand, color= "red", size = 9.5, material = "suede", condition = "tattered")
+        @brand = brand
+        @color = color
+        @size = size
+        @material = material
+        @condition = condition
+    end
+
+    def cobble
+        puts "Your shoe is as good as new!"
+        @condition = "new"
+    end
+end
+
+shoe = Shoe.new("Nike", "red", 9.5, "suede", "tattered")
+# binding.pry
